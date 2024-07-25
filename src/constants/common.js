d8f4345a7c54cc4abff62f9438aff7f775831120
Josue Zavala <josue.zavala.projects@gmail.com>
2024-07-24 19:23:00 -0600

    feat(constants): Adding common file

---
+++ src/constants/common.js
@@ -0,0 +1,57 @@
+export const JSON_BOOKS = [
+  {
+    id: 1,
+    image:
+      "https://m.media-amazon.com/images/I/71DWY1ioreL._AC_UF894,1000_QL80_.jpg",
+    name: "In Search Of Lost Time",
+    author: "Marcel Proust",
+    year: 1913,
+    rating: 9.8,
+    buy_link:
+      "https://www.amazon.com.mx/Search-Lost-Time-Vol-Budding/dp/1841598968",
+  },
+  {
+    id: 2,
+    image:
+      "https://m.media-amazon.com/images/I/71DWY1ioreL._AC_UF894,1000_QL80_.jpg",
+    name: "Don Quixote",
+    author: "Miguel De Cervantes",
+    year: 1605,
+    rating: 9.7,
+    buy_link:
+      "https://www.amazon.com.mx/Search-Lost-Time-Vol-Budding/dp/1841598968",
+  },
+  {
+    id: 3,
+    image:
+      "https://m.media-amazon.com/images/I/71DWY1ioreL._AC_UF894,1000_QL80_.jpg",
+    name: "Ulysses",
+    author: "James Joyce",
+    year: 1904,
+    rating: 9.5,
+    buy_link:
+      "https://www.amazon.com.mx/Search-Lost-Time-Vol-Budding/dp/1841598968",
+  },
+  {
+    id: 4,
+    image:
+      "https://m.media-amazon.com/images/I/71DWY1ioreL._AC_UF894,1000_QL80_.jpg",
+    name: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    year: 1925,
+    rating: 9.4,
+    buy_link:
+      "https://www.amazon.com.mx/Search-Lost-Time-Vol-Budding/dp/1841598968",
+  },
+  {
+    id: 5,
+    image:
+      "https://m.media-amazon.com/images/I/71DWY1ioreL._AC_UF894,1000_QL80_.jpg",
+    name: "Moby Dick",
+    author: "Herman Melville",
+    year: 1851,
+    rating: 9.3,
+    buy_link:
+      "https://www.amazon.com.mx/Search-Lost-Time-Vol-Budding/dp/1841598968",
+  },
+];
